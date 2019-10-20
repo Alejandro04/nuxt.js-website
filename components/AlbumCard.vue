ed66e28bc02fbcb6517ab24defd46e60953ef3b5
Alejandro Roa <a.roa@fixme.es>
2019-10-20 11:09:48 -0500

    init

---
+++ components/AlbumCard.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="column is-one-quarter">
+    <div class="box">
+      <h3>{{album.title}}</h3>
+      <a href="#" class="button is-primary">ver más</a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AlbumCard',
+  props: {
+    album: Object
+  }
+}
+</script>
+
+<style lang="css">
+.box {
+  padding: 2em;
+}
+h3{
+  min-height: 5em;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="container">
+    <div class="title">Álbumes</div>
+    <div class="columns is-multiline">
+      <AlbumCard :album="album" v-for="album in albums" :key="album.id" />
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+import env from '../config/env'
+import AlbumCard from '../components/AlbumCard'
+
+export default {
+  mounted () {
+    axios.get(`${env.endpoint}/albums`).then((response) => {
+      this.albums = response.data
+    })
+  },
+  components: {
+    AlbumCard
+  },
+  data () {
+    return {
+      albums: []
+    }
+  }
+}
+</script>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
